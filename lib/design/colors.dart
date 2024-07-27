843479d2274bdd173b71e524c7ac3bc661182e97
Igor Meshkov <48246916+RuLap@users.noreply.github.com>
2024-07-27 18:49:48 +0500

    Added status line

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -5,6 +5,7 @@ const Color secondaryColor = Color(0xFFF4F8FB);
 const Color secondaryVariantColor = Color(0xFF00D6BD);
 const Color backgroundColor = Color(0xFFF2F2F2);
 const Color surfaceColor = Color(0xFFFFFFFF);
+const Color secondaryBackgroundColor = Color(0xFFCCE4FC);
 
 const Color connectedColor = Color(0xFF00FF00);
 const Color disconnectedColor = Color(0xFFFF0000);
--- lib/pages/product/status_line.dart
+++ lib/pages/product/status_line.dart
@@ -1,16 +1,88 @@
-import 'package:flutter/material.dart';
-import 'package:products_app/design/colors.dart';
+// ignore_for_file: library_private_types_in_public_api
 
-class StatusLine extends StatelessWidget {
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+import '../../application/product_service.dart';
+import '../../design/colors.dart';
+
+class StatusLine extends StatefulWidget {
   const StatusLine({super.key});
 
   @override
+  _StatusLine createState() => _StatusLine();
+}
+
+class _StatusLine extends State<StatusLine> {
+  late final ProductService productService;
+  late Future<bool> _currentConnection;
+
+  @override
+  void initState() {
+    super.initState();
+    productService = const ProductService();
+    _currentConnection = productService.ping();
+    setUpTimedFetch();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Row(children: [
-      Icon(
-        Icons.lens,
-        color: connectedColor),
-      Text("Connected")
-    ]);
+    return FutureBuilder(
+      future: _currentConnection,
+      builder: (context, snapshot) {
+        if(snapshot.connectionState == ConnectionState.waiting) {
+          return const Text("Waiting for server response");
+        }
+        else if(snapshot.hasData) {
+          return _statusRow(snapshot.data!);
+        }
+        else {
+          return _statusRow(false);
+        }
+      } 
+    );
+  }
+
+  Widget _statusRow(bool status) {
+    return Container(
+      color: secondaryBackgroundColor,
+      child: _status(status),
+    );
+  }
+
+  Widget _status(bool status) {
+    if(status) {
+      return const Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            Icons.lens,
+            color: connectedColor,
+          ),
+          Text("Connected")
+        ]
+      );
+    }
+    else {
+      return const Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            Icons.lens,
+            color: disconnectedColor,
+          ),
+          Text("Disconnected")
+        ]
+      );
+    }
+  }
+
+  setUpTimedFetch() {
+    Timer.periodic(const Duration(seconds: 10), (timer) {
+      setState(() {
+        _currentConnection = productService.ping();
+      });
+    });
   }
 }
