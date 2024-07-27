81b0133eaa4f1a8a8db9b4af1dea35e85b315c6f
Igor Meshkov <meshkov-74@yandex.ru>
2024-07-27 15:00:22 +0500

    Model init

---
+++ lib/domain/product.dart
@@ -0,0 +1,47 @@
+import 'dart:typed_data';
+
+class Product {
+  String name;
+  String description;
+  ProductImage image;
+  double price;
+
+  Product({
+    required this.name,
+    required this.description,
+    required this.image,
+    required this.price
+  });
+
+  factory Product.fromJson(Map<dynamic, dynamic> json) => Product(
+    name: json["name"],
+    description: json["description"],
+    image: ProductImage.noImage(selectBaseImage(json["images_ids"])),
+    price: json["price"].toDouble()
+  );
+
+  static String selectBaseImage(List<dynamic> images) {
+    for(String image in images) {
+      if(image.isNotEmpty) {
+        return image;
+      }
+    }
+
+    return "";
+  }
+}
+
+class ProductImage {
+  String name;
+  Uint8List? bytes;
+
+  ProductImage({
+    required this.name,
+    this.bytes
+  });
+
+  factory ProductImage.noImage(String imageName) => ProductImage(
+    name: imageName,
+    bytes: null
+  );
+}
